6f44bb869ddbf49cc7d75356ab8ff50d3e2cbcf1
life-random <80086175+life-random@users.noreply.github.com>
2021-04-09 15:49:53 +0900

    Add files via upload

---
+++ listEX.py
@@ -0,0 +1,10 @@
+fstName = ['C', 'h', 'o', 'i']
+sndName = ["Byung", "Kwon"]
+stdNum = [2, 0, 2, 1, 0, 6, 0, 3, 7]
+
+print(fstName)
+print(sndName)
+# 화면에 C, B, K를 출력하는 프로그램을 작성하시오
+print(fstName[0])   #C
+print(sndName[0][0])#B
+print(sndName[1][0])#K
