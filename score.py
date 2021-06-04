91d1e33a0333886b0e55f56c2b211289d3060e7b
life-random <80086175+life-random@users.noreply.github.com>
2021-06-04 13:56:29 +0900

    Delete score.py

--- score.py
+++
@@ -1,8 +0,0 @@
-score = int(input("성적 입력 : "))  # 성적 입력
-
-if(score >= 60):            # ()안에 결과가 True이면
-    print("합격입니다!")
-    print("축하합니다!")
-else:                       # ()안에 결과가 False이면
-    print("불합격입니다.")
-    print("안타깝습니다. 내년에 봐요")
