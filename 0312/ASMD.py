6395fd7cab0085903019db7575bf616db15755de
life-random <80086175+life-random@users.noreply.github.com>
2021-06-04 13:58:14 +0900

    Add files via upload

---
+++ 0312/ASMD.py
@@ -0,0 +1,9 @@
+#문자열 입력
+a = input("첫 번째 숫자 : ")
+b = input("두 번째 숫자 : ")
+print(a+b)
+print("hello" + "python")
+# 두 정수를 입력받다 사칙연산 결과를 출력
+a = int(input("첫 번째 숫자 : "))
+b = int(input("두 번째 숫자 : "))
+print(a+b)
